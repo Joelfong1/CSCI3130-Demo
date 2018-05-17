bbe2db1bac96fdb6d91c586a6e2d0216f669f11b
Joelfong1 <Joelfong1@gmail.com>
2018-05-17 15:54:47 -0300

    A1 Complete
    Adds a button and a text field to the UI. The text can be inputted and submitted with the button to change the viewable text

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText textInput;
+    private TextView textOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submit = findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                textInput = findViewById(R.id.inputText);
+                textOutput = findViewById(R.id.outputText);
+                textOutput.setText(textInput.getText().toString());
+            }
+        });
     }
 }
